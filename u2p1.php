e3b2dee416f5377dd181ba4d0ca8819b71c540f2
sandeepvasita1 <sandeepvasita93@gmail.com>
2026-09-24 14:52:12 +0530

    Update u2p1.php
    Added PHP script to set and display a cookie named 'username'.

--- u2p1.php
+++ u2p1.php
@@ -1,3 +1,6 @@
+Write a PHP script to set a cookie named "username" with 
+the value "Welcome" with an expiration time of one hour 
+and display its value. 
 <?php
 setcookie("username", "Welcome", time() + 3600);
 
